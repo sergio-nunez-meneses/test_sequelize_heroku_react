70c74b7d77cb1b36af0a9a5db8f22024a3e4db9b
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-10-15 00:19:12 +0200

    added markers fetched from API but with random coordinates

--- Components/FarmersMap.js
+++ Components/FarmersMap.js
@@ -6,21 +6,88 @@ import {
   TouchableOpacity,
   TextInput
 } from 'react-native';
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 export default class FarmersMap extends React.Component {
   constructor(props) {
     super(props);
     this.state =
     {
-      region :
+      region:
       {
-        latitude: 1.290270,
-        longitude: 103.851959,
-        latitudeDelta: 0.02,
-        longitudeDelta: 0.02,
-      }
+        latitude: this.minMaxRandom(47, 47.1),
+        longitude: this.minMaxRandom(3.1, 3.2),
+        latitudeDelta: 0.045,
+        longitudeDelta: 0.045,
+      },
+      markers: [
+        {
+          coordinate: {
+            latitude: this.minMaxRandom(47, 47.1),
+            longitude: this.minMaxRandom(3.1, 3.2),
+          },
+          title: "Best Place",
+          description: "Description1",
+          id: 1
+        },
+        {
+          coordinate: {
+            latitude: this.minMaxRandom(47, 47.1),
+            longitude: this.minMaxRandom(3.1, 3.2),
+          },
+          title: "Best Place",
+          description: "Description2",
+          id: 2
+        }
+      ],
+      data: []
     }
+  }
+
+  getFarmers = async() => {
+    const response = await fetch('https://gerundio-farmers.herokuapp.com/API/displayAll');
+    const farmers = await response.json();
+    this.setState({ data: farmers.farmers });
+  };
+
+  componentDidMount = async() => {
+    const response = await fetch('https://gerundio-farmers.herokuapp.com/API/displayAll');
+    const farmers = await response.json();
+    this.setState({ data: farmers.farmers });
+  };
+
+  randomMarkers = () => {
+    //
+  }
+
+  mapMarkers = () => {
+    return this.state.markers.map(
+      (markers) =>
+      <Marker
+        key={markers.id}
+        coordinate={{ latitude: markers.coordinate.latitude, longitude: markers.coordinate.longitude }}
+        title={markers.title}
+        description={markers.description}
+      >
+      </Marker>
+    )
+  }
+
+  farmerMarkers = () => {
+    return this.state.data.map(
+      (data) =>
+      <Marker
+        key={data.id}
+        coordinate={{ latitude: this.minMaxRandom(47, 47.1), longitude: this.minMaxRandom(3.1, 3.2) }}
+        title={data.name}
+        description={data.address}
+      >
+      </Marker>
+    )
+  }
+
+  minMaxRandom = (min, max) => {
+    return Math.random() * (max - min + 1) + min;
   }
 
   home = () => {
@@ -36,8 +103,16 @@ export default class FarmersMap extends React.Component {
           showsUserLocation = {true}
           showsCompass = {true}
           rotateEnabled = {true}
+          loadingEnabled = {true}
+          loadingIndicatorColor="#666666"
+          loadingBackgroundColor="#eeeeee"
+          moveOnMarkerPress = {false}
+          showsUserLocation={true}
+          showsCompass={true}
           style={{ flex: 10, alignItems: 'center' }}
-        />
+        >
+          {this.farmerMarkers()}
+        </MapView>
         <View style={styles.TouchableOpacityStyleContainer}>
           {/* <TextInput
             placeholder="search a farmer"
@@ -66,7 +141,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   TouchableOpacityStyleContainer: {
-    flex: 2.25,
+    flex: 1.25, // 2.25 with TextInput
     alignItems: 'center',
     paddingTop: 5
   },
